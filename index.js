95069bf4113302a79e7c15f5fae57e08a80ff3f8
freefmly <freefmly@gmail.com>
2020-03-11 20:54:24 +0900

    d

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ const PORT = 4000;
 const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
 
-const handleHome = (req, res) => res.send("Hello frodm Home");
+const handleHome = (req, res) => res.send("Hello from Home");
+
 const handleProfile = (req, res) => res.send("Hello I'm Profile");
 
-app.get("/", handleHome);
+const betweenHome = () => console.log("i'm between");
+
+app.get("/", betweenHome, handleHome);
 
 app.get("/profile", handleProfile);
 
