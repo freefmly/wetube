b2e4f4f48ad9d919f5c0b4df5ef74ddfa785191f
freefmly <freefmly@gmail.com>
2020-03-12 01:45:45 +0900

    making server

--- app.js
+++ app.js
@@ -3,17 +3,12 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express();
-
-const PORT = 4000;
-
-const handleListening = () =>
-  console.log(`Listening on: http://localhost:${PORT}`);
-
-const handleHome = (req, res) => res.send("Hello from Home");
-
-const handleProfile = (req, res) => res.send("Hello I'm Profile");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -21,8 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(helmet());
 
-app.get("/", handleHome);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
-app.get("/profile", handleProfile);
-
-app.listen(PORT, handleListening);
+export default app;
