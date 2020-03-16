00d34bad5dfbc69a95ac4e1dc5de8713c4f2532d
freefmly <freefmly@gmail.com>
2020-03-16 21:17:42 +0900

    up

---
+++ db.js
@@ -0,0 +1,54 @@
+export const videos = [
+  {
+    id: 324393,
+    title: "Video awesome",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "리프리",
+      email: "freefmly@naver.com"
+    }
+  },
+  {
+    id: 121231,
+    title: "Video super",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "리프리",
+      email: "freefmly@naver.com"
+    }
+  },
+  {
+    id: 41254233,
+    title: "Video nice",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "리프리",
+      email: "freefmly@naver.com"
+    }
+  },
+  {
+    id: 546878,
+    title: "Video perfect",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "리프리",
+      email: "freefmly@naver.com"
+    }
+  }
+];
